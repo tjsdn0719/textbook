fd1923228b0ed998841f71a736b666ed000bf2c4
cob <sun0719wooda@gmail.com>
2023-07-03 20:27:57 +0900

    p144문제 풀이및정답

---
+++ p144-answer.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main()
+{
+	int n, sum = 0;
+	printf("용의자의 DNA 정보를 입력하세요.  : ");
+	scanf("%d", &n);
+	while(n!=0)
+	{
+		sum += n % 10;
+		n = n/10;
+	 } 
+	 if(sum % 7 == 4)
+	 	printf("범인\n");
+	else 
+		printf("일반인\n");
+		return 0;
+}
